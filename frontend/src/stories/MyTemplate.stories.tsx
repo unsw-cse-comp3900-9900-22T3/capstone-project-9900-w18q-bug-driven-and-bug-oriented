3dceafd9ce5af7bea9ca43a4fc52cc81519cb568
spyxgr <107198839+spyxgr@users.noreply.github.com>
2022-09-30 17:40:58 +1000

    modify template

--- frontend/src/stories/MyTemplate.stories.tsx
+++ frontend/src/stories/MyTemplate.stories.tsx
@@ -3,9 +3,9 @@ import { ComponentStory, ComponentMeta } from "@storybook/react";
 import MyTemplate from "./MyTemplate";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
-
+//修改所有的MyTemplate(不要修改Template)
 export default {
-  title: "Template",
+  title: "MyTemplate",
   component: MyTemplate,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
@@ -18,7 +18,9 @@ const Template: ComponentStory<typeof MyTemplate> = (args) => (
 );
 
 export const Test = Template.bind({});
+// 参数,storybook里的默认参数
 Test.args = {
   props1: "111",
   props2: "222",
+  props3: true,
 };
--- frontend/src/stories/MyTemplate.tsx
+++ frontend/src/stories/MyTemplate.tsx
@@ -2,21 +2,32 @@ import { Box } from "@mui/system";
 import React from "react";
 import { Button } from "@mui/material";
 
+// 声明变量的数据格式
 interface ListProps {
+  //问号是说可有可无
   props1?: string;
   props2?: string;
+  props3?: boolean;
+  //预留空函数
+  doSomething?: (params: any) => any;
 
 }
 
+
 export default function Template({
+  // 参数，内容影响不大可以没有（如果return要用的话，必须声明）
   props1 = '',
   props2 = '',
+  props3 = true,
+  doSomething,
+
   ...props
 }: ListProps) {
   return (
     <>
       <Box>{props1}</Box>
       <Button>{props2}</Button>
+      <div>{props3}</div>
     </>
   );
 }
