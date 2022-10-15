8e95ee340c933a60414f30672da7bfd6b7ec35ae
spyxgr <107198839+spyxgr@users.noreply.github.com>
2022-10-15 18:25:37 +1100

    1

--- frontend/src/stories/manager/ManagerOrderCard.tsx
+++ frontend/src/stories/manager/ManagerOrderCard.tsx
@@ -12,7 +12,6 @@ interface ListProps {
   orderId?: string;
   table?: string;
   time?: string;
-  status?: string;
   price?: string;
   itemList?: {
     dishName: string,
@@ -21,34 +20,18 @@ interface ListProps {
   }[];
 
 
-
   //预留空函数
   confirmFunc: (params: any) => any;
 
 }
 
-const style = {
-  position: 'absolute' as 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  height: 280,
-  bgcolor: 'background.paper',
-  borderRadius: 3,
-  boxShadow: 24,
-  p: 4,
-  pt: 2,
-};
-
 
 // 别忘了修改函数名
-export default function OrderCard({
+export default function ManagerOrderCard({
   // 参数，内容影响不大可以没有（如果return要用的话，必须声明）
   orderId = '',
   table = '',
   time = '',
-  status = '',
   price = '',
   itemList = [],
   confirmFunc,
@@ -56,16 +39,8 @@ export default function OrderCard({
   ...props
 }: ListProps) {
 
-  const [open, setOpen] = React.useState(false);
   // const [day, setDay] = React.useState('');
   // const [t, setT] = React.useState('');
-
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-  const handleComfirm = (e: any) => {
-    setOpen(false);
-    confirmFunc(e);
-  };
 
   // useEffect(() => {
 
@@ -91,9 +66,14 @@ export default function OrderCard({
   return (
     <>
       <Card sx={{ minHeight: 275, width: 450, backgroundColor: '#F7F7F7', borderRadius: 5 }}>
-        <Typography sx={{ m: 2, ml: 4, fontWeight: 'bold' }} variant='h6'>
-          Table {table}
-        </Typography>
+      <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+          <Typography sx={{ m: 2, ml: 4, fontWeight: 'bold' }} variant='h6'>
+            Table {table}
+          </Typography>
+          <Typography sx={{ color: '#626264', m:2, mr:4 }} variant='subtitle1'>
+            #{orderId}
+          </Typography>
+        </Box>
         <Box sx={{ display: 'flex', flexDirection: 'row', mb: 2 }}>
           <Typography sx={{ ml: 4, color: '#626264' }} variant='subtitle1'>
             <CalendarTodayIcon sx={{ mr: 0.5, mb: -0.5 }} />{day}
@@ -142,72 +122,7 @@ export default function OrderCard({
           Total: ${price}
         </Typography>
         <Box sx={{ width: '100%', justifyContent: 'center', display: 'flex', mt: 5, mb: 5 }}>
-          {status === 'yes' && (
-            <>
-              <Button onClick={handleOpen} variant="contained" color='warning' sx={{ width: 190, borderRadius: 2, mr: 3 }}>Pay</Button>
-              <Modal
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="modal-modal-title"
-                aria-describedby="modal-modal-description"
-              >
-
-                <Card sx={style}>
-                  <Box sx={{ display: 'flex', justifyContent: 'right', marginRight: -2 }}>
-                    <IconButton onClick={handleClose} color="primary" sx={{ color: '#A3A3A4' }} aria-label="upload picture" component="label">
-                      <ClearIcon />
-                    </IconButton>
-                  </Box>
-
-                  <Box sx={{ justifyContent: 'center', display: 'flex', mt: 3 }}>
-                    <Typography variant="h4" sx={{ fontWeight: 'bold' }} >
-                      Confirm to Pay?
-                    </Typography>
-
-                  </Box>
-                  <Box sx={{ justifyContent: 'center', display: 'flex', mt: 3 }}>
-                    <Typography sx={{ color: '#626264' }} variant='subtitle2'>
-                      Have you received the payment?
-                    </Typography>
-                  </Box>
-                  <Box sx={{ justifyContent: 'center', display: 'flex', }}>
-                    <Typography sx={{ color: '#626264' }} variant='h6'>
-                      Table {table},   Total: ${price}
-                    </Typography>
-                  </Box>
-
-                  <Box sx={{ display: 'flex', justifyContent: 'center', mt: 4 }}>
-                    <Button onClick={handleComfirm} sx={{
-                      width: 150, '&:hover': {
-                        backgroundColor: '#8475B0',
-                      }, backgroundColor: '#503E9D', fontWeight: 'bold', height: 55, borderRadius: 3, mr: 5
-                    }}>
-                      <Typography variant="h6" sx={{ color: '#ffffff' }} >
-                        Confirm
-                      </Typography>
-                    </Button>
-                    <Button onClick={handleClose} sx={{
-                      width: 150, '&:hover': {
-                        backgroundColor: '#F1F1F1',
-                      }, backgroundColor: '#F7F7F7', fontWeight: 'bold', height: 55, borderRadius: 3,
-                    }}>
-                      <Typography variant="h6" sx={{ color: '#000000', }} >
-                        cancel
-                      </Typography>
-                    </Button>
-                  </Box>
-                </Card>
-
-              </Modal>
-            </>
-
-
-          )}
-          {status === 'no' && (
-            <Button variant="contained" color='warning' disabled sx={{ width: 190, borderRadius: 2, mr: 3 }}>Pay</Button>
-          )}
-
-          <Button onClick={handleChange} variant="contained" color='warning' sx={{ width: 190, borderRadius: 2 }}>details</Button>
+          <Button onClick={handleChange} variant="contained" color='warning' sx={{ width: 400, borderRadius: 2 }}>details</Button>
         </Box>
       </Card>
     </>
