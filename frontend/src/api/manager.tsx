a8ec5ec4d93e03825a6dd77fa03fcbd378cdfd3b
spyxgr <107198839+spyxgr@users.noreply.github.com>
2022-10-28 16:52:06 +1100

    1

--- frontend/src/api/manager.tsx
+++ frontend/src/api/manager.tsx
@@ -10,3 +10,63 @@ export const postManagerCategory =async (input:{'categoryName':string}) => {
   const result = await apiClient.post(`/manager/category/add`,data);
   return result.data;
 }
+
+export const getManagerItem =async () => {
+  const result = await apiClient.get(`/manager/item`);
+  return result.data;
+}
+
+export const addManagerItem =async (input:{
+  categoryName:string;
+  title:string;
+  description:string;
+  ingredient:string;
+  cost:number;
+  calorie:number;
+  picture:string;
+}) => {
+  const data = JSON.stringify(input);
+  const result = await apiClient.post(`/manager/item/add`,data);
+  return result.data;
+}
+
+export const deleteManagerItem =async (id:string) => {
+  const result = await apiClient.post(`/manager/item/${id}`);
+  return result.data;
+}
+
+export const editManagerItem =async (id:string, input:{
+  categoryName:string;
+  title:string;
+  description:string;
+  ingredient:string;
+  cost:number;
+  calorie:number;
+  picture:string;
+}) => {
+  const data = JSON.stringify(input);
+  const result = await apiClient.put(`/manager/item/${id}`,input);
+  return result.data;
+}
+
+export const getManagerKey =async () => {
+  const result = await apiClient.get(`/manager/key`);
+  return result.data;
+}
+
+export const postManagerKey =async (input:{
+  role:string;
+  name:string;
+  key:string;
+}) => {
+  const result = await apiClient.post(`/manager/key`,input);
+  return result.data;
+}
+
+export const deleteManagerKey =async (input:{
+  role:string;
+  key:string;
+}) => {
+  const result = await apiClient.delete(`/manager/key`,{data:{input}});
+  return result.data;
+}
--- frontend/src/pages/ManagerMenu.tsx
+++ frontend/src/pages/ManagerMenu.tsx
@@ -6,21 +6,83 @@ import {
 } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import NavBar from "../stories/NavBar";
+import { getManagerItem } from "../api/manager";
 
 
 const theme = createTheme();
 
+interface categoryInterface {
+  categoryName: string;
+  categoryId: number;
+}
+
+interface itemInterface {
+  itemList: {
+    ingredient: string;
+    picture: string;
+    categoryName: string;
+    dishId: number;
+    description: string;
+    calorie: number;
+    dishName: string;
+    price: number;
+  }
+}
+
 const ManagerMenu: React.FC<{}> = () => {
   const navigate = useNavigate();
+  const [categoryList, setCategoryList] = useState<categoryInterface>();
+  const [itemList, setItemList] = useState();
+
+  const init = async () => {
+    const message = await getManagerItem();
+    if (message) {
+      setCategoryList(message.categoryList);
+      setItemList(message.itemList);
+    }
+
+  }
 
   return (
     <ThemeProvider theme={theme}>
       <Box sx={{ height: '100vh', display: 'flex', flexDirection: 'row' }}>
-        <NavBar role='manager' doSomething={()=>{}} postRequest={() => { }} />
-    
-        <div>ManagerMenu page</div>
+        <Box>
+          <NavBar role='manager' doSomething={() => { }} postRequest={() => { }} />
+        </Box>
+
+
+        <Box sx={{ height: '100vh', width: '100%', display: 'flex', flexDirection: 'column' }}>
+          <Box sx={{ alignItems: 'end', justifyContent: 'right', height: 200, width: '100%', display: 'flex' }}>
+            <Box sx={{ mr: 20 }}>
+              <div>test</div>
+            </Box>
+
+          </Box>
+          <Box sx={{ height: '100%', width: '100%', overflow: "auto", mt: 5 }}>
+            {
+              // categoryList?.categoryList.map((item) => {
+              //   return (
+              //     <React.Fragment key={item.categoryId}>
+              //       <Box sx={{ my: 3, mx: 10, height: 185 }}>
+              //         <ManagerCategoryCard
+              //           categoryId={item.categoryId}
+              //           categoryName={item.categoryName}
+              //           lastModified={item.lastModified}
+              //           canMove={moveItem === item.categoryId ? true : false}
+              //           fatherListener={setMoveItem}
+              //         />
+              //       </Box>
+
+              //     </React.Fragment>
+              //   )
+              // })
+            }
+          </Box>
+        </Box>
+
+
       </Box>
-      
+
     </ThemeProvider>
   );
 };
